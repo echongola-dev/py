a7428aabfa8a8dab5025d25c7c65860dec4b892f
Edio Iberico Chongola <71687136+echongola-dev@users.noreply.github.com>
2021-10-14 08:46:44 +0200

    Add files via upload

---
+++ py/Aulaspy/CursoAula08.py
@@ -0,0 +1,11 @@
+from math import sqrt, floor
+num = int(input('Digite um numero: '))
+raiz = sqrt(num)
+print('A raiz de {} e igual {}'.format(num, floor(raiz)))
+
+# Duas formas de importar bibliotecas
+
+# import math 
+# num = int(input('Digite um numero: '))
+# raiz = sqrt(num)
+# print('A raiz de {} e igual {}'.format(num, floor(raiz)))
