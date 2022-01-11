ac16f4bec834f7f1008c4bde2664c3b5a428520b
Edio Iberico Chongola <71687136+echongola-dev@users.noreply.github.com>
2022-01-11 20:17:38 +0200

    Add files via upload

---
+++ py/ExerciciosPy/ex061.py
@@ -0,0 +1,21 @@
+# quando usamos o for
+
+'''primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razao: '))
+decimo = primeiro + (10 - 1) * razao
+for c in range(primeiro, decimo + razao, razao):
+    print('{} '.format(c), end='-> ')
+print('ACABOU')'''
+
+# quando usamos o While
+print('Gerador de PA')
+print('-=' * 10)
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razao da PA: '))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print(' {} -> '.format(termo), end='')
+    termo += razao
+    cont += 1
+print('FIM')
